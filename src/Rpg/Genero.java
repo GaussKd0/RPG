2a731fabf1d287004133c20d214a49a97aad968c
Pedro Galvão <93468007+GaussKd0@users.noreply.github.com>
2022-10-04 02:35:38 -0300

    Create Genero.java

---
+++ src/Rpg/Genero.java
@@ -0,0 +1,42 @@
+ackage Rpg;
+import java.util.Scanner;
+public class Genero {
+
+	public static String ReceberGenero;
+
+	public static String Genero() {
+		int i = 2;		
+		int sc;
+		Scanner scan = new Scanner(System.in);
+		String Genero;
+		System.out.print("Qual Vai Ser O Genero Do Seu Personagem {1} Homen {2} Mulher");
+		while(i == 2) {
+
+			sc = scan.nextInt();
+
+			if(sc == 1) {
+				i = 5;
+				System.out.println("Seu Genero É Homen");
+				Genero = "Homen";
+				ReceberGenero = Genero;
+
+			}
+			else if (sc == 2) {
+				i = 5;
+				System.out.println("Seu Genero É Mulher");
+				Genero = "Mulher";
+				i = 0;
+				ReceberGenero = Genero;
+			}
+			else {
+				System.out.println("Voce Não Escolheu Um Genero");
+
+			}
+
+
+		}
+		return ReceberGenero;
+
+	}
+
+}
